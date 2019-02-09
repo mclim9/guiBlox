18745e7584d24150688f9fc615e8081d95ed5c11
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-02-09 09:00:41 -0800

    clean up.

--- GUI/main.py
+++ GUI/main.py
@@ -5,38 +5,41 @@ from entryCol   import entryCol
 from initGui    import theme
 from listWindow import listWindow
 
-def testprint1():
-    print("root.entryCol.entry0.text")
-
-def testprint2(root):
+def testprint1(root):
     print(root.entryCol.entry0.get())
 
+def testprint2(root):
+    root.bottWind.clear()
+    
 def testprint3(root):
-    root.listWind.wwrite('asdf')
+    root.bottWind.wwrite('asdf')
 
 def main(): 
     root = theme().addColor()
+    root.title('GUI Example')
 
     ### Create Sections
-    root.buttnRow = buttonRow(root, 3)                     #pylint: disable=unused-variable
     root.entryCol = entryCol(root, ['SMW IP','FSW IP','Freq Start','Freq Stop','Freq Step'])
     root.toppWind = listWindow(root)
     root.bottWind = listWindow(root)
     root.bottWind.stdOut()                                  #Stdout --> window
+    root.buttnRow = buttonRow(root, 3)                      #pylint: disable=unused-variable
 
     ### Define Sections
-    root.buttnRow.button0.config(text='foo',command=testprint1)
-    root.buttnRow.button1.config(text='bar',command=lambda: testprint2(root))
-    root.buttnRow.button2.config(text='baz',command=lambda: testprint3(root))
-    root.toppWind.listWindow.config(height=10,width=60)
-    root.bottWind.listWindow.config(height= 5,width=85)
-    root.buttnRow.frame.config(width=85)
+    root.entryCol.frame.config(width=100)
+    root.toppWind.listWindow.config(height=10,width=40)
+    root.bottWind.listWindow.config(height= 5,width=66)
+#    root.buttnRow.frame.config(width=)
+    root.buttnRow.button0.config(text='foo'  ,command=lambda: testprint1(root))
+    root.buttnRow.button1.config(text='clear',command=lambda: testprint2(root))
+    root.buttnRow.button2.config(text='baz'  ,command=lambda: testprint3(root))
 
     ### Grid Sections
-    root.entryCol.frame.grid(row=0,column=0,sticky=(tk.N,tk.S))
+    root.grid_rowconfigure(2, weight=1)
+    root.entryCol.frame.grid(row=0,column=0,sticky="ns")
     root.toppWind.frame.grid(row=0,column=1)
     root.bottWind.frame.grid(row=1,column=0,columnspan=2)
-    root.buttnRow.frame.grid(row=2,column=0,columnspan=2,sticky=(tk.W,tk.E))
+    root.buttnRow.frame.grid(row=2,column=0,columnspan=2,sticky="nsew")
     root.mainloop()
 
 if __name__ == '__main__':
