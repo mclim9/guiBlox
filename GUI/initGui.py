5492e13069107465cc672aee0e5511d8662d4221
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-03-01 14:14:44 -0800

    experiment w/ init

--- GUI/initGui.py
+++ GUI/initGui.py
@@ -2,11 +2,10 @@ import tkinter as tk
 
 class theme():
     def __init__(self):
-        #self = tk.Tk()
+        self = tk.Tk()
         self.btn = {}
         self.btn['width'] = 10
         self.btn['height'] = 10
-        pass
         
     def addColor(self):
         self = tk.Tk()
@@ -15,6 +14,7 @@ class theme():
         self.clr['txtBg']    = "black" 
         self.clr['appFg']    = 'white'
         self.clr['appBg']    = "grey30"
+        self.config(bg=self.clr['appBg'])
         return self
 
 if __name__ == '__main__':
