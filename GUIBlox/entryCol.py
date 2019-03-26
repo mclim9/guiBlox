6eecc2d9b47a2eb8fc5088f670869817f8e7d18a
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-03-26 16:40:26 -0700

    add entryCol1 to display single column

--- GUIBlox/entryCol.py
+++ GUIBlox/entryCol.py
@@ -35,6 +35,22 @@ class entryCol:
                 outDict[child._name] = child.get()
         return outDict
 
+class entryCol1:
+    def __init__(self, master, names):
+        self.master = master
+        self.frame = tk.Frame(master)
+        self.frame.config(bg=master.clr['appBg'])
+        for i,key in enumerate(names):
+            if i == 0:
+                setattr(self, f'label{i}', tk.Label(self.frame,width=15, bg=master.clr['appBg'], fg=master.clr['appFg'], text=key))
+                getattr(self,f'label{i}').grid(row=i,column=0)
+            setattr(self, f'entry{i}', tk.Entry(self.frame,width=15, bg=master.clr['txtBg'], fg=master.clr['txtFg']))
+            getattr(self,f'entry{i}').insert(tk.END,names[key])
+            getattr(self,f'entry{i}').grid(row=i+1,column=0,sticky='ew')
+            getattr(self,'frame').grid_rowconfigure(i+1, weight=1)
+        self.frame.grid(column=1,sticky="nsew")
+#        self.frame.grid_columnconfigure(1, weight=1)
+
 if __name__ == '__main__':
     root = theme.theme().addColor()
     root.geometry(f'{300}x{300}')
@@ -45,5 +61,5 @@ if __name__ == '__main__':
     app = entryCol(root,dictIn)                     #pylint: disable=unused-variable
     app.frame.config(width=100, height=100)
     app.frame.grid(row=0,column=0,sticky="nsew")
-    asdf = app.save()
+    print(app.save())
     root.mainloop()
