1b9a6b8e4db8c754da25232cb2f0aa196600d028
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2022-01-18 17:04:34 -0800

    Version 2022.1.18

--- upload.bat
+++ upload.bat
@@ -1,5 +1,6 @@
 python setup.py sdist
 set datestr=%date:~10,4%.%date:~4,2%.%date:~7,2%
 echo %datestr%
-python -m unittest -v test.test_guiblox
+python -m unittest -v test.test_guiblox_import
+python -m unittest -v test.test_project_import
 twine upload .\dist\guiblox-*.tar.gz
